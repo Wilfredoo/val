c4112e60f6eb474568f8d66cfad31364967cdaf0
wilfredoo <cacao_de_verdad@protonmail.com>
2022-07-24 19:29:39 +0200

    Updates

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <p>
+        Portfolio of Valerie Stoll
+        </p>
+    </div>
+  );
+}
+
+export default App;

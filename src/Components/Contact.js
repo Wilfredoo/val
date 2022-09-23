d85e3ac731c505b5372f36149a2030f711b91d9a
wilfredoo <cacao_de_verdad@protonmail.com>
2022-09-23 10:53:52 +0200

    .

---
+++ src/Components/Contact.js
@@ -0,0 +1,13 @@
+
+function Contact() {
+    return (
+      <div className="App">
+          <p>
+          wir sind in Kontakt Seite
+          </p>
+      </div>
+    );
+  }
+  
+  export default Contact;
+  
--- src/index.js
+++ src/index.js
@@ -24,12 +24,14 @@ import App from './App';
 import  Home  from "./Components/Home";
 import Test1  from "./Components/Test1";
 import  Test2  from "./Components/Test2";
+import  Contact  from "./Components/Contact";
 
 render(
   <BrowserRouter>
     <Routes>
       <Route exact path="test1" element={<Test1 />} />
       <Route exact path="test2" element={<Test2 />} />
+      <Route exact path="contact" element={<Contact />} />
       <Route exact path="/home" element={<Home />} />
       <Route exact path="/" element={<App />} />
       <Route path="*" element={<main>404</main>} />
